f1dc640b19b0b7a4de7065e1c56cc31fa0103d36
fitgeek96 <fitgeek96@gmail.com>
2023-05-22 15:37:11 +0300

    Displaying Platform Icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,48 @@
+import { Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    nintendo: SiNintendo,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    web: BsGlobe,
+  };
+  return (
+    <>
+      {platforms.map((platform) => (
+        <Icon
+          as={iconMap[platform.slug]}
+          marginTop={1}
+          marginRight={2}
+          color="gray.500"
+        >
+          {platform.slug}
+        </Icon>
+      ))}
+    </>
+  );
+};
+
+export default PlatformIconList;
