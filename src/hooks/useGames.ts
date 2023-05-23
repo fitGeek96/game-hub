8fdb96fa46eb888859a1e29c06ef110e4f241186
fitgeek96 <fitgeek96@gmail.com>
2023-05-23 13:45:35 +0300

    Show Loading Skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,15 +24,21 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false)
   
     useEffect(() => {
       const controller = new AbortController()
+
+      setLoading(true)
       apiClient
         .get<FetchGamesResponse>("/games", { signal: controller.signal})
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results)
+          setLoading(false)})
         .catch((err) => {
           if (err instanceof CanceledError) return;
           setError(err.message)
+          setLoading(false)
         });
   
       return () => controller.abort();
@@ -42,7 +48,8 @@ const useGames = () => {
         games,
         setGames,
         error,
-        setError
+        setError,
+        isLoading
     }
 }
 
